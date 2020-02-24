b6e9f0c2c334a462604ebea6a96aa5446a5cefd0
wassim.znaidi <wassim.znaidi@irt-systemx.fr>
2020-02-24 15:10:01 +0100

    - refactor: enhance the logging with chalk color

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,14 +1,48 @@
-export function l(msg: string | Error) {
+import * as chalk from 'chalk';
+
+export function l(msg: string | object | Error) {
     if (typeof msg === 'string') {
-        console.log(`[BNC] - ${msg}`);
+        console.log(chalk.blue(`[BNC] - ${msg}`));
+    } if (typeof msg === 'object') {
+        console.log(chalk.blue('[BNC] - ', msg));
     } else {
         if ((msg as any).responses && (msg as any).responses.length > 0) {
             for (let response of (msg as any).responses) {
-                console.log(response.Error);
-                console.log(response);
+                console.log(chalk.red(response.Error));
+                console.log(chalk.red(response));
             }
         } else {
-            console.log(msg);
+            console.log(chalk.red(msg));
         }
+    }
+}
+
+export function e(msg: string | object) {
+    switch (typeof msg) {
+        case 'string': {
+            console.log(chalk.red(`[BNC] - ${msg}`));
+            break;
+        }
+        case 'object': {
+            console.log(chalk.red('[BNC] - ', msg));
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+export function d(msg: string | object) {
+    switch (typeof msg) {
+        case 'string': {
+            console.log(chalk.green(`[BNC] - ${msg}`));
+            break;
+        }
+        case 'object': {
+            console.log(chalk.green('[BNC] - ', msg));
+            break;
+        }
+        default:
+            break;
     }
 }
