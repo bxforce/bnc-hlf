0f9aaea59283d0c9a723a4cafeb26f78e29061cb
wassim.znaidi <wassim.znaidi@irt-systemx.fr>
2020-03-02 14:54:48 +0100

    - fix: add sudo prompt

--- src/generators/crypto/createCA.sh.ts
+++ src/generators/crypto/createCA.sh.ts
@@ -1,22 +1,24 @@
+import * as chalk from 'chalk';
+import * as sudo from 'sudo-prompt';
 import { BaseGenerator } from '../base';
 import { DockerComposeYamlOptions } from '../../utils/data-type';
 
 export class CreateCAShGenerator extends BaseGenerator {
   contents = `
-export REGISTRAR_DIR=$PWD
+export REGISTRAR_DIR=${this.options.networkRootPath}
 export FABRIC_CA_CLIENT_HOME=${this.options.networkRootPath}/fabric-binaries/${this.options.envVars.FABRIC_VERSION}/bin
 
-export PATH=FABRIC_CA_CLIENT_HOME:$PWD:$PATH
-export FABRIC_CFG_PATH=$PWD
+export PATH=FABRIC_CA_CLIENT_HOME:${this.options.networkRootPath}:$PATH
+export FABRIC_CFG_PATH=${this.options.networkRootPath}
 
-export ORG_DIR=$PWD/crypto-config/peerOrganizations/${this.options.org.fullName}
-export PEER_TLS=$PWD/peertls
+export ORG_DIR=${this.options.networkRootPath}/crypto-config/peerOrganizations/${this.options.org.fullName}
+export PEER_TLS=${this.options.networkRootPath}/peertls
 export PEER_DIR=$ORG_DIR/peers/${this.options.org.firstPeerFullName}
 export REGISTRAR_DIR=$ORG_DIR/users/admin
 export ADMIN_DIR=$ORG_DIR/users/Admin@${this.options.org.fullName}
 export TLS=$ORG_DIR/tlsca
 mkdir -p $ORG_DIR/ca $ORG_DIR/msp $PEER_DIR $REGISTRAR_DIR $ADMIN_DIR $TLS
-mkdir certsICA
+mkdir ${this.options.networkRootPath}/certsICA
 
 fabric-ca-client enroll -m admin -u http://adminCA:adminpw@ca.root:7054 
 
@@ -28,10 +30,32 @@ fabric-ca-client register --id.name ca.interm.${this.options.org.fullName} --id.
 
 docker-compose -f docker-compose-ca.yaml up -d ca.interm.${this.options.org.fullName}
 
-cp -r $PWD/certsICA/* $ORG_DIR/ca
+# cp -r ${this.options.networkRootPath}/certsICA/* $ORG_DIR/ca
 `;
 
   constructor(filename: string, path: string, private options: DockerComposeYamlOptions) {
     super(filename, path);
+  }
+
+  copyAsRoot() {
+    const options = {
+      name: 'BNC'
+    };
+
+    const command = `cp -r ${this.options.networkRootPath}/certsICA/* $ORG_DIR/ca`;
+
+    return new Promise((resolved, rejected) => {
+      sudo.exec(command, options, (error, stdout, stderr) => {
+        if (error) {
+          rejected(error);
+        }
+
+        if (stderr) {
+          console.error(chalk.red(stderr));
+        }
+
+        resolved(true);
+      });
+    });
   }
 }
